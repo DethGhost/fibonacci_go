7db9d1eac50fc2a300d4b76e48e404ff7790a00e
dethghost <admin@deth.org.ua>
2018-06-12 11:38:55 +0300

    make refactoring

--- Main.go
+++ Main.go
@@ -4,10 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"time"
 	"log"
 	"strings"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -16,59 +16,8 @@ func main() {
 		"It's simple game where you need enter numbers in fibonacci sequence.\n" +
 		"Game will be ended when you enter correct 10 numbers or make 3 mistakes.\n" +
 		"You have 10 seconds for each answer. Game will be start when you enter 'start' enter 'exit' for exit")
-	input := make(chan string, 1)
-	fImput, _ := bufio.NewReader(os.Stdin).ReadString('\n');
-	go getInput(input)
-	userError := 0
-	userOk := 0
-start:
-	if strings.TrimRight(fImput, "\n") == "exit" {
-		fmt.Println("bye")
-		os.Exit(1)
-	} else if strings.TrimRight(fImput, "\n") == "start" {
-		fmt.Println("Yuor game is begining, enter first fibonacci number")
-		Fibonacci := fibonacci()
-		currentNum := 0
-		for i := 0; i <= 10; i++ {
-			currentNum = Fibonacci()
-			select {
-			case res := <-input:
-				enteredNum, _ := strconv.Atoi(res);
-				if currentNum != enteredNum {
-					userError++
-					fmt.Errorf("You have enter wrong number")
-					fmt.Printf("{%d:%d} \n", i, currentNum)
-					fmt.Printf("You have %d tries\n", 3-userError)
-					if userError == 3 {
-						fmt.Println("GAME OVER")
-						fmt.Println("For start game enter 'start' for exit 'exit'")
-						fImput, _ = bufio.NewReader(os.Stdin).ReadString('\n');
-						strings.TrimRight(fImput, "\n");
-						goto start
-					}
-				}
-				userOk++
-				if userOk == 10{
-					fmt.Println("Congratulation you have enter all numbers \n" +
-						"YOU WIN")
-					fmt.Println("For start game enter 'start' for exit 'exit'")
-					fImput, _ = bufio.NewReader(os.Stdin).ReadString('\n');
-					strings.TrimRight(fImput, "\n");
-					goto start
-				}
-				break
-			case <-time.After(10 * time.Second):
-				fmt.Println("bye")
-				os.Exit(1)
-				break
-			}
-		}
-	} else {
-		fmt.Println("Your enter is wrong. Enter 'start' or 'exit'")
-		fImput, _ = bufio.NewReader(os.Stdin).ReadString('\n');
-		strings.TrimRight(fImput, "\n");
-		goto start
-	}
+
+	gameFlow()
 
 }
 
@@ -82,6 +31,71 @@ func fibonacci() func() int {
 	}
 }
 
+func gameFlow() {
+
+	fImput, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	fImput = strings.TrimRight(fImput, "\r\n")
+	userError := 0
+	userOk := 0
+
+	if strings.TrimRight(fImput, "\r\n") == "exit" {
+		fmt.Println("bye")
+		os.Exit(1)
+	} else if strings.TrimRight(fImput, "\r\n") == "start" {
+		fmt.Println("Yuor game is begining, enter first fibonacci number")
+		Fibonacci := fibonacci()
+		position := 0
+		for {
+			position++
+			a := game(userError, userOk, position, Fibonacci())
+			userError = a[1]
+			userOk = a[0]
+			if userOk == 10 {
+				fmt.Println("Congratulation you have enter all numbers \n" +
+					"YOU WIN")
+				fmt.Println("For start game enter 'start' for exit 'exit'")
+break
+			} else if userError == 3 {
+				fmt.Println("GAME OVER")
+				fmt.Println("For start game enter 'start' for exit 'exit'")
+				break
+			}
+		}
+
+	} else {
+		fmt.Println("Your enter is wrong. Enter 'start' or 'exit'")
+	}
+	gameFlow()
+}
+
+func game(userError int, userOk int, position int, currentNum int) []int {
+
+	input := make(chan string, 1)
+	go getInput(input)
+	fmt.Println(currentNum)
+	select {
+	case res := <-input:
+		enteredNum, _ := strconv.Atoi(res);
+		fmt.Println(enteredNum)
+		if currentNum != enteredNum {
+			userError++
+			fmt.Println("You have enter wrong number")
+			fmt.Printf("{%d:%d} \n", position, currentNum)
+			fmt.Printf("You have %d tries\n", 3-userError)
+		} else {
+			userOk++
+		}
+		break
+	case <-time.After(10 * time.Second):
+		userError++
+		fmt.Println("Times up")
+		fmt.Printf("{%d:%d} \n", position, currentNum)
+		fmt.Printf("You have %d tries\n", 3-userError)
+		break
+	}
+	return []int{userOk, userError}
+}
+
 func getInput(input chan string) {
 	for {
 		in := bufio.NewReader(os.Stdin)
@@ -90,6 +104,6 @@ func getInput(input chan string) {
 			log.Fatal(err)
 		}
 
-		input <- strings.TrimRight(result, "\n")
+		input <- strings.TrimRight(result, "\r\n")
 	}
 }
